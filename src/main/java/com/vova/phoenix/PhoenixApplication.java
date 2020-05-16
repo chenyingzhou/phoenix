ad5bd63ed6608ab6f58b35d0139cab7ebf3dabe0
chenyingzhou <echocyz@gmail.com>
2020-05-16 10:58:44 +0800

    处理下日期相关的东西

--- src/main/java/com/vova/phoenix/PhoenixApplication.java
+++ src/main/java/com/vova/phoenix/PhoenixApplication.java
@@ -4,9 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @MapperScan("com.vova.phoenix.mapper")
 public class PhoenixApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PhoenixApplication.class, args);

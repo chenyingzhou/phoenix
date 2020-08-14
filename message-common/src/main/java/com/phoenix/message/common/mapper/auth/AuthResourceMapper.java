8e5801e1fd7f50124904c2b118820b89e2c07034
chenyingzhou <echocyz@gmail.com>
2020-08-14 17:59:46 +0800

    加上resource

---
+++ message-common/src/main/java/com/phoenix/message/common/mapper/auth/AuthResourceMapper.java
@@ -0,0 +1,7 @@
+package com.phoenix.message.common.mapper.auth;
+
+import com.phoenix.message.common.entity.AuthResource;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface AuthResourceMapper extends Mapper<AuthResource> {
+}
---
+++ message-common/src/main/java/com/phoenix/message/common/mapper/auth/AuthUserResourceMapper.java
@@ -0,0 +1,7 @@
+package com.phoenix.message.common.mapper.auth;
+
+import com.phoenix.message.common.entity.AuthUserResource;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface AuthUserResourceMapper extends Mapper<AuthUserResource> {
+}

7187fd352bec2cd3d7071a9ef5980a2a9b61a92e
chenyingzhou <echocyz@gmail.com>
2020-09-13 14:41:59 +0800

    clinet-demo

--- message-web/src/main/java/com/phoenix/message/web/controller/PushController.java
+++ message-web/src/main/java/com/phoenix/message/web/controller/PushController.java
@@ -5,22 +5,35 @@ import com.phoenix.message.common.constant.SqlConstant;
 import com.phoenix.message.common.entity.PushTaskConfig;
 import com.phoenix.message.common.filter.PushTaskConfigFilter;
 import com.phoenix.message.common.util.JacksonUtil;
+import com.phoenix.message.grpc.PushServiceGrpc;
+import com.phoenix.message.grpc.RpcFindPushTaskConfigListReq;
+import com.phoenix.message.grpc.RpcFindPushTaskConfigListResp;
 import com.phoenix.message.web.model.BaseListResponse;
 import com.phoenix.message.web.model.BaseResponse;
 import com.phoenix.message.web.model.request.PushTaskConfigReq;
 import com.phoenix.message.web.model.response.PushTaskConfigRsp;
+import io.grpc.Channel;
+import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
 public class PushController extends BaseController {
 
+    @GrpcClient("admin-service")
+    private Channel serverChannel;
+
     @GetMapping("/push/task-config")
     public BaseListResponse<PushTaskConfigRsp> taskConfigList(PushTaskConfigFilter pushTaskConfigFilter, Pagination pagination) {
+
+        PushServiceGrpc.PushServiceBlockingStub stub = PushServiceGrpc.newBlockingStub(serverChannel);
+        RpcFindPushTaskConfigListResp resp = stub.rpcFindPushTaskConfigList(RpcFindPushTaskConfigListReq.newBuilder().setAppName("vova").build());
+
         pagination.setOrderBy(PushTaskConfig.ID);
         pagination.setOrderType(SqlConstant.ORDER_DESC);
         List<PushTaskConfig> pushTaskConfigList = pushService.findTaskConfigList(pushTaskConfigFilter, pagination);
